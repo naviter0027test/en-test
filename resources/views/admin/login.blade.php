8c9fbc168ffbc45d342900ebefe0795af80cac95
Lanker <naviter0027test@gmail.com>
2019-10-25 09:27:24 +0800

    登入、題庫設定等頁面初始

---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>英文測驗系統</title>
+        <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+        <link href='lib/bootstrap/dist/css/bootstrap.min.css' rel='stylesheet' />
+        <link href='lib/bootstrap/dist/css/bootstrap-theme.min.css' rel='stylesheet' />
+        <link href='css/admin/login.css' rel='stylesheet' />
+    </head>
+    <body>
+        <form action="/admin/setting">
+            <h3>登入</h3>
+            <input type="password" name="" />
+            <button>登入</button>
+        </form>
+    </body>
+    <script src="lib/jquery-2.1.4.min.js"></script>
+    <script src="js/admin/login.js"></script>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,31 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/admin/login', function() {
+    return view('admin.login');
+});
+
+Route::get('/admin/setting', function() {
+    return view('admin.setting.index');
+});
+
+Route::get('/admin/setting/exam', function() {
+    return view('admin.setting.exam');
+});
+
+Route::get('/admin/setting/info', function() {
+    return view('admin.setting.info');
+});
+
+Route::get('/admin/questions', function() {
+    return view('admin.questions.index');
+});
+
+Route::get('/admin/result', function() {
+    return view('admin.result.index');
+});
+
+Route::get('/admin/logout', function() {
+    return view('admin.login');
+});
