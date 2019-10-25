e95e34128796a807913005cc25bd19129002f5d6
Lanker <naviter0027test@gmail.com>
2019-10-25 11:10:08 +0800

    登入與管理者設定頁面製作

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -3,17 +3,19 @@
         <meta charset="utf-8">
         <title>英文測驗系統</title>
         <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
-        <link href='lib/bootstrap/dist/css/bootstrap.min.css' rel='stylesheet' />
-        <link href='lib/bootstrap/dist/css/bootstrap-theme.min.css' rel='stylesheet' />
-        <link href='css/admin/login.css' rel='stylesheet' />
+        <link href='/lib/bootstrap/dist/css/bootstrap.min.css' rel='stylesheet' />
+        <link href='/lib/bootstrap/dist/css/bootstrap-theme.min.css' rel='stylesheet' />
+        <link href='/css/admin/body.css' rel='stylesheet' />
+        <link href='/css/admin/login.css' rel='stylesheet' />
     </head>
     <body>
-        <form action="/admin/setting">
-            <h3>登入</h3>
-            <input type="password" name="" />
-            <button>登入</button>
+        <form class="loginForm" action="/admin/setting">
+            <h3><i class="glyphicon glyphicon-globe" ></i> 登入</h3>
+            <h5>Password:</h5>
+            <p> <input type="password" name="" /> </p>
+            <p class="loginBtnP"> <button>登入</button> </p>
         </form>
     </body>
-    <script src="lib/jquery-2.1.4.min.js"></script>
-    <script src="js/admin/login.js"></script>
+    <script src="/lib/jquery-2.1.4.min.js"></script>
+    <script src="/js/admin/login.js"></script>
 </html>
--- resources/views/admin/setting/index.blade.php
+++ resources/views/admin/setting/index.blade.php
@@ -1,1 +1,30 @@
-setting index
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>英文測驗系統</title>
+        <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+        <link href='/lib/bootstrap/dist/css/bootstrap.min.css' rel='stylesheet' />
+        <link href='/lib/bootstrap/dist/css/bootstrap-theme.min.css' rel='stylesheet' />
+        <link href='/css/admin/body.css' rel='stylesheet' />
+        <!-- <link href='/css/admin/setting/index.css' rel='stylesheet' /> -->
+    </head>
+    <body>
+        <div class="header">
+            <a href="/admin/setting" class="clicked">管理者設定</a>
+            <a href="/admin/setting/exam">應試相關設定</a>
+            <a href="/admin/questions">題庫</a>
+            <a href="/admin/result">應試成績相關</a>
+            <a href="/admin/setting/info">相關說明</a>
+            <a href="/admin/logout">登出</a>
+        </div>
+        <div class="content">
+            <h3><i class="glyphicon glyphicon-globe" ></i> 管理者設定</h3>
+            <form class="contentForm1" action="./">
+                <p><span>管理者密碼</span><input type="password" /></p>
+                <button>儲存</button>
+            </form>
+        </div>
+    </body>
+    <script src="/lib/jquery-2.1.4.min.js"></script>
+    <script src="/js/admin/login.js"></script>
+</html>
